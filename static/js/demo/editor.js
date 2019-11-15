a857ef9e95510e2d578eeec2d8b4c52d16be8f13
Prateek Tulsyan <prateek.tulsyan@decurtis.com>
2019-11-15 19:04:30 +0000

    indent

--- static/js/demo/editor.js
+++ static/js/demo/editor.js
@@ -4,10 +4,10 @@ var yasqe = YASQE(document.getElementById("yasqe"), {
         endpoint: "http://localhost:8890/sparql/"
     }
 });
+
 var yasr = YASR(document.getElementById("yasr"), {
 	//this way, the URLs in the results are prettified using the defined prefixes in the query
 	getUsedPrefixes: yasqe.getPrefixesFromQuery
 });
 
-//link both together
 yasqe.options.sparql.callbacks.complete = yasr.setResponse;
